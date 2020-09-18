1f91e536c899c775f88b60d1b1015ea0555ed8ed
802653 <54371363+802653@users.noreply.github.com>
2020-09-18 10:36:14 -0700

    agd
    gas

--- project1/doms.js
+++
@@ -1,9 +0,0 @@
-function GameArea(){
-    //  Wrapper Div
-    this.wrapperDiv = document.getElementById("wrapperDiv");
-    this.wrapperDiv.setAttribute("style", " background-color:yellow; border: 5px solid black; width:900px; height:800px;");
-    // create tileMenuDiv
-    this.tileMenuDiv = document.createElement("div");
-    this.wrapperDiv.appendChild(this.tileMenuDiv)
-    this.tileMenuDiv.setAttribute("style", " background-color:#033c4a; width:900px; height:100px;float:left;");
-}
