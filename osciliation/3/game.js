d8c03483f92bcd012f258ea34ec82b904e0264e7
802653 <54371363+802653@users.noreply.github.com>
2020-10-22 19:56:58 -0700

    hds
    sdh

---
+++ osciliation/3/game.js
@@ -0,0 +1,72 @@
+function Game(){
+
+    this.gamePaused = false;    // the game may be paused or not
+    this.ga = new GameArea();   // create all the dom elements
+    // get the canvas as a property of the game
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/canvas
+    this.canvas = document.getElementById('canvas');
+    // get the context
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+    this.ctx = this.canvas.getContext('2d'); // This is the context
+
+    //   create the array of bubble objects
+	this.movers = [];   
+    this.numMovers = 55;
+	this.createMovers(this.canvas,this.numMovers);
+
+	//this.movers.push(new Bubble(this.canvas.width/2, this.canvas.height/2, 0, 0, 130, clr));
+
+    //  Add event handlers to all tile objects
+    for(let i = 0; i < this.ga.tiles.length; i++){
+        this.ga.tiles[i].addEventListener('mouseover', // mouseover is the name of an event
+                                        function(){//  JavaScript has anonymous functions
+                                          //  'this' is the listener target object: tile
+                                          //  'this' does not refer to the game object
+                                          this.style.backgroundColor = "#ac8fe3"
+                                        },
+                                        false);
+        this.ga.tiles[i].addEventListener('mouseout', function(){
+            this.style.backgroundColor = "#d5dee0"
+          },false);
+        this.ga.tiles[i].addEventListener('click', function(){
+            game.gamePaused = !game.gamePaused;
+            console.log("Mouse Clicked");
+          },false);
+    }
+
+}//++++++++++++++++++++++  end movers constructor
+
+// function to run the game each animation cycle
+Game.prototype.run = function(){
+  if(!this.gamePaused){
+	
+		for(let i=0; i <this.movers.length;i++) {
+			if (this.movers[i]!= null) {
+					this.movers[i].run();
+			}
+		}
+	}
+}
+
+Game.prototype.createMovers = function(canvas, numMovers) {
+
+    for(var i = 0; i < this.numMovers; i++){
+        var x, y, dx, dy, diam, clr, r, g, b;
+        x = Math.random()*this.canvas.width;
+        y = Math.random()*this.canvas.height;
+        dx = Math.random()*5-2.5;
+        dy = Math.random()*5-2.5;
+        diam = Math.random()*20;
+        r = 255;
+        g = 255;
+        b = 255;
+		numOrbs = Math.floor(Math.random*5+3);
+        clr = "rgba(" + r + ", "+ g + ","+ b +")"
+        this.movers.push(new Mover(x, y, dx, dy, diam, clr, numOrbs)); // add new bubble to array
+		//this.movers.push(new Triangle(x+80, y+80, dx, dy, diam, clr)); // add new bubble to array
+    }
+	
+	
+}
+
+
---
+++ osciliation/3/mover.js
@@ -0,0 +1,119 @@
+
+
+//  Mover constructor function +++++++++++++++++++++++++++++
+
+function Mover(x, y, dx, dy, rad, clr,orbs){
+	this.location = new JSVector(x,y);
+    this.velocity = new JSVector(dx,dy);
+	this.acceleration = new JSVector(0,0);
+	this.rad = rad;
+	this.mass = 0.31415 * rad * rad;
+	if(this.rad < 100) {
+			this.mass = 100
+	}
+	this.clr = clr;
+	this.isOverlapping = false;
+	
+	this.f = null;
+}
+
+  //  placing methods in the prototype (every Mover shares functions)
+
+Mover.prototype.run = function(){
+	this.velocity.add(this.acceleration);
+    this.location.add(this.velocity);
+	this.checkOverlapping();
+	this.update();
+	this.render();
+	this.checkEdges();
+	this.acceleration.multiply(0);
+	this.velocity.x += -0.00002 * 2 * 3.1415 * this.rad *  this.velocity.x
+	this.velocity.y += -0.00002 * 2 * 3.1415 * this.rad * this.velocity.y
+}
+
+Mover.prototype.applyForce = function(force) {
+	this.f = force.copy();
+	this.f.divide(this.mass);
+    this.acceleration.add(this.f); 
+	
+}
+
+Mover.prototype.gravity = function(a) {
+	force = JSVector.subGetNew(this.location, a.location); 
+	let distance = force.getMagnitude();
+	if (distance < 20) {
+		distance = 20;
+	}
+	//if (distance > 200) {
+	//	distance = 200;
+	//}
+	//force.normalize();
+	let strength = (2 * this.mass * a.mass)/ (distance*distance);
+	force.setMagnitude(strength);
+	return force;
+	
+}
+
+// check if this Mover is overlapping any other Mover
+Mover.prototype.checkOverlapping = function(){
+    this.isOverlapping = false;//  default color
+    this.clr =  "rgba(255,255,255,255)"
+    let b = game.movers;
+    for(let i = 0; i < b.length; i++){ // for all the Movers
+       if(this !== b[i]){   // if not this Mover
+         let d = Math.sqrt((this.location.x-b[i].location.x)*(this.location.x-b[i].location.x) + (this.location.y-b[i].location.y)*(this.location.y-b[i].location.y));
+         if(d < this.rad + b[i].rad){
+            this.isOverlapping = true;
+            this.clr =  "rgba(100,220,55,10)"
+			let vCollision = {x: this.location.x - b[i].location.x, y: this.location.y - b[i].location.y};
+              let vRelativeVelocity = {x: this.velocity.x - b[i].velocity.x, y: this.velocity.y - b[i].velocity.y};
+              let vCollisionNorm = {x: vCollision.x / d, y: vCollision.y / d};
+              let speed = vRelativeVelocity.x * vCollisionNorm.x + vRelativeVelocity.y * vCollisionNorm.y;
+
+              let impulse = 2 * speed / (this.mass + b[i].mass);
+              this.velocity.x -= (impulse * b[i].mass * vCollisionNorm.x);
+              this.velocity.y -= (impulse * b[i].mass * vCollisionNorm.y);
+			  
+              b[i].velocity.x += (impulse * this.mass * vCollisionNorm.x);
+              b[i].velocity.y += (impulse * this.mass * vCollisionNorm.y);
+			  
+			  this.location.add(this.velocity);
+			  b[i].location.add(b[i].velocity);
+         }
+       }
+    }
+
+  }
+
+// draw the Mover on the canvas
+Mover.prototype.render = function(){
+    let ctx = game.ctx;
+    // color depends on whether this Mover overlaps any oher Mover
+	if(!this.isOverlapping) {
+		ctx.strokeStyle = "rgba(255,155,255,255)"//this.clr;
+	}
+	else {
+		ctx.strokeStyle = "rgba(255,155,155,255)"//this.clr;
+	}
+    ctx.fillStyle = this.clr;
+    ctx.beginPath();
+	ctx.arc(this.location.x, this.location.y, this.rad, 0, 2 * Math.PI);
+	ctx.stroke();
+
+  }
+
+// Move the Mover in a random direction
+Mover.prototype.update = function(){
+    //if(!game.gamePaused){
+    //  this.velocity.setMagnitude(Math.random()*4-8);
+    //}
+  }
+
+// When a Mover hits an edge of the canvas, it wraps around to the opposite edge.
+Mover.prototype.checkEdges = function(){
+    let canvas = game.canvas;
+    if(this.location.x > canvas.width)  this.velocity.multiply(-1); // wrap around from right to left
+    if(this.location.x < 0)  this.velocity.multiply(-1); // wrap around from left to right
+    if(this.location.y > canvas.height)  this.velocity.multiply(-1); // wrap around from bottom to top
+    if(this.location.y < 0)  this.velocity.multiply(-1); // wrap around from top to bottom
+  }
